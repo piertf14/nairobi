7f18bb2323966e5f38a4397d6d656baabca36ed3
Piero Terreros <piertf14@gmail.com>
2019-05-01 23:50:43 -0500

    heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn nairobi.wsgi --timeout 10000 --log-file -
--- nairobi/settings.py
+++ nairobi/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import dj_database_url
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -75,10 +75,7 @@ WSGI_APPLICATION = 'nairobi.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
 
 
