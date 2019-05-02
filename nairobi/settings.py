4c2c4f1ac2d0eec9807626e95fa42e9d9a18cff1
Piero Terreros <piertf14@gmail.com>
2019-05-01 23:54:19 -0500

    Add allowed host

--- nairobi/settings.py
+++ nairobi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fwyl&10^40ls14b!!+&q_1-q=gaf&+ipw#2nvn-2v$8fzk!=)2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
